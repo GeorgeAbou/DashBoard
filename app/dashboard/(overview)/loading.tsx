1a50248b4dcb73ba67718300fc8dd1ccbc96f2cb
George <abuchegeorge@gmail.com>
2024-08-13 11:04:07 -0400

    dashboard page load

---
+++ app/dashboard/(overview)/loading.tsx
@@ -0,0 +1,7 @@
+
+//versión simplificada de la interfaz de usuario,es el esqueleto de los compoentes de la page.tsx pero sin el layout
+import DashboardSkeleton from '@/app/ui/skeletons';
+ 
+export default function Loading() {
+  return <DashboardSkeleton />;
+}
--- app/dashboard/page.tsx
+++
@@ -1,8 +0,0 @@
-
-
-
-//pagina principal que se carga al colocar la ruta/dashboard y se carga a la vez con el layout, 
-//ya que el layout es fijo de la carpeta de toda la raiz
-export default function Page() {
-    return <p>Dashboard Page</p>;
-  }
